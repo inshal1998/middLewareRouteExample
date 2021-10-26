c838569c35847afa61ab4f02b99606c3555e17ea
Inshal Ansari <inshalansari80@gmail.com>
2021-10-26 15:48:15 +0530

    Some Fixes

--- middleware/middle.js
+++ middleware/middle.js
@@ -3,9 +3,9 @@ function admin(req,res,next) {
     if (req.params.name && req.params.name == admin || req.body.name && req.body.name == admin) {
         res.send(`<h1>HEllo ${admin}</h1>`)
     } else {
-        res.send(`<h1>Kaun Hai bei tu ${req.params.name != null ?req.params.name : req.body.name }</h1>`)
+        res.send(`<h1>Kaun Hai bei tu ${req.params.name != null ?req.params.name : req.body.name } jaa ${admin} Boss sei baat kar</h1>`)
     }
-    console.log("This is From MiddleWare :  ",req.params.name);
+    console.log("This is From MiddleWare :  ",req.params.name , `jaa ${admin} sei baat kar`);
     next()
 }
 
